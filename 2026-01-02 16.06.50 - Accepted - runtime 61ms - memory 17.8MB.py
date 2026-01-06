606467740c3fea82645e317644fe4d241b82c9cb
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-06 11:44:23 +0530

    Initial commit

---
+++ 2026-01-02 16.06.50 - Accepted - runtime 61ms - memory 17.8MB.py
@@ -0,0 +1,21 @@
+from threading import Barrier, Semaphore
+
+class H2O:
+    def __init__(self):
+        self.barrier = Barrier(3)
+        self.h_sem = Semaphore(2)
+        self.o_sem = Semaphore(1)
+
+    def hydrogen(self, releaseHydrogen: 'Callable[[], None]') -> None:
+        self.h_sem.acquire()
+        self.barrier.wait()
+        # releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen()
+        self.h_sem.release()
+
+    def oxygen(self, releaseOxygen: 'Callable[[], None]') -> None:
+        self.o_sem.acquire()
+        self.barrier.wait()
+        # releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen()
+        self.o_sem.release()
